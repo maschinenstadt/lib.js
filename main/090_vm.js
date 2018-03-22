427b11cc829bb317a6eaa552b0121c505d19c315
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-22 22:30:45 +0100

    vm preparations and links

---
+++ main/090_vm.js
@@ -0,0 +1,22 @@
+var main = module.exports = {};
+
+/*
+ * for isolated js-apps .. maybe server-side-js @ httpd? ;-)´
+ *
+ * 	... see also @ "doc/txt/VM.txt". ..
+ *
+ *
+ * < http://www.davidmclifton.com/2011/08/18/node-js-virtual-machine-vm-usage/ >
+ * < https://60devs.com/executing-js-code-with-nodes-vm-module.html >
+ * < https://www.hacksparrow.com/scripting-a-node-js-app.html >
+ *
+ */
+
+//TODO/
+//..
+//global.vm = ..
+
+
+//
+console.debug(2, "Loaded 'vm'");
+
