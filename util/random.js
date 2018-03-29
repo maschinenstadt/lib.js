27d54f3f6c3c59247852bc3fa6e5d821676b68f1
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-29 20:15:43 +0200

    util/random BROWSER AND LINUX *READY*

--- util/random.js
+++ util/random.js
@@ -5,39 +5,93 @@ random.length = 1024;
 //TODO/ also in BROWSER!???
 random.encoding = [ 'binary', 'hex', 'base64' ];
 
-//ONLY LINUX NOW.. TODO (windows etc)!!
-random.file = '/dev/urandom';
-
 if(BROWSER)
 {
+	random.crypto = window.crypto || window.msCrypto;
+
 	random.random = function(_length = random.length, _encoding = random.encoding[0])
 	{
+		if(not(random.crypto))
+		{
+			//TODO??/ if window.message doesn't exist.. what else, altert??
+			//
+			//NOW: using "return _error". better for now.. (check if worx in browser, maybe @ onload-catch?
+			//
+			//.. or should i GLOBALLY use the WEB.CONSOLE(S)!?!?!!?
+			//
+			var errMessage = '"crypto" not available (in your browser)!!';
+			return new Error(errMessage);
+		}
+
 		if(! type(_length, 'Number'))
 		{
 			_length = random.length;
 		}
-		if(! type(_encoding, 'String'))
+
+		var encType = type(_encoding);
+
+		if(encType === 'Number')
+		{
+			if(_encoding < 2 || _encoding > 36)
+			{
+				_encoding = random.encoding[0];
+				encType = 'String';
+			}
+		}
+		else if(encType !== 'String')
 		{
 			_encoding = random.encoding[0];
 		}
 
 		var result = '';
+		var iterations = 0;
 
-		switch(_encoding)
+		while(result.length < _length)
 		{
-			case 'hex':
-				break;
-			case 'base64':
-				break;
-			case 'binary':
-			default:
-				break;
+			iterations++;
+
+			var str = '';
+			var buffer = new Uint8Array(_length);
+
+			crypto.getRandomValues(buffer);
+
+			for(var i = 0; i < buffer.length; i++)
+			{
+				if(encType === 'Number')
+				{
+					// various systems (default js)
+					str += buffer[i].toString(_encoding);
+				}
+				else if(encType === 'String')
+				{
+					// my own '_encoding's
+					switch(_encoding)
+					{
+						case 'hex':
+							str += buffer[i].toString(16);
+							break;
+						case 'base64':
+						case 'binary':
+							str += String.fromCharCode(buffer[i]);
+					}
+				}
+			}
+
+			if(_encoding === 'base64')
+			{
+				str = window.btoa(str);
+			}
+
+			result += str;
 		}
 
-		if(result.length > _length)
+		if(iterations !== 1)
 		{
-			result = result.substr(0, _length);
+			var errMessage = '"util/random": random() iterations not equal (1)! CHECK THIS!';
+			return new Error(errMessage); // or throw!?? must be catch'ed .. maybe @ window.onload..
 		}
+
+		result = result.substr(0, _length);
 
 		return result;
 	}
@@ -62,6 +116,17 @@ if(BROWSER)
 }
 else
 {
+	/*
+	 * THIS IS *LINUX* ONLY (using '/dev/urandom')!!!
+	 *
+	 * TODO is the implementation for WINDOWS etc. .. i think about using node.js' "crypto" module (getBytes() or so)!!
+	 * determine os and think further..! ;-)´
+	 * (also if it's really good to use /dev/urandom ... instead of node.js' "crypto" module? or use it globally, also for LINUX??!?
+	 *
+	 */
+	random.crypto = '/dev/urandom';
+
+	//TODO/ maybe also radix >=2 && <= 36? see BROWSER impl..
 	random.random = function(_length = random.length, _encoding = random.encoding[0])
 	{
 		if(! global.type(_length, 'Number'))
@@ -76,19 +141,32 @@ else
 		// Don't forget to shorten/pad result, by _encoding (producing various lengths)
 		var result = '';
 
-		switch(_encoding)
+		// this 'while' is just to be sure... regularily it should work in once (see the "iterations" number variable..)
+		var iterations = 0
+		while(result.length < _length)
 		{
-			case 'hex':
-				result = global.file.readBytes.hex(random.file, _length);
-				break;
-			case 'base64':
-				result = global.file.readBytes.base64(random.file, _length);
-				break;
-			case 'binary':
-			default:
-				result = global.file.readBytes.binary(random.file, _length);
+			iterations++;
+
+			switch(_encoding)
+			{
+				case 'hex':
+					result += global.file.readBytes.hex(random.crypto, _length);
+					break;
+				case 'base64':
+					result += global.file.readBytes.base64(random.crypto, _length);
+					break;
+				case 'binary':
+				default:
+					result += global.file.readBytes.binary(random.crypto, _length);
+			}
 		}
 
+		if(iterations !== 1)
+		{
+			console.warning('"util/random": random() iterations not equal (1)! CHECK THIS!');
+		}
+
+		// SHOULD REGULARILY ALSO result in wished _length .. see "global.file.readBytes()" ;-)´
 /*		if(result.length > _length)
 		{
 			result = result.substr(0, _length);
@@ -112,8 +190,6 @@ else
 		return random.random(_length, 'base64');
 	}
 
-	// linux impl.
-	// //TODO/ windows!?!?
 	module.exports = random;
 }
 
