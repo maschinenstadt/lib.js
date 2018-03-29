838cd87ec1491081f9c37c64d78873eb998bc181
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-29 12:48:44 +0200

    w/ "once" now (causes error!)

--- example/node/event.js
+++ example/node/event.js
@@ -4,8 +4,13 @@ var node = include('core/node');
 node = new node();
 
 node.event.on('log', console.log);
-node.event.on('error', console.error);
+node.event.once('error', console.error);
 
 node.event.emit('log', 'console.log');
 node.event.emit('error', 'console.error');
+
+timer.sleep(2000);
+
+node.event.emit('log', 'console.log#2');
+node.event.emit('error', 'console.error#2');
 
