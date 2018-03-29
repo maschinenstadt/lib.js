b160f23916fde0bc2d7ddd64eb0b41090fdba9ad
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-29 13:55:09 +0200

    w/ net encoding

--- settings.js
+++ settings.js
@@ -2,6 +2,9 @@ var settings = module.exports = {};
 
 settings.default = 'sync' || 'async';
 
+settings.encoding = 'utf8';
+settings.charset = 'utf-8';
+
 settings.version = {};
 settings.version.check = true;	// throw a message if newer version is available ..
 settings.version.url = 'https://nodejs.org/dist/latest/';
@@ -11,13 +14,11 @@ settings.version.delay = 30;	// in minutes .. will be converted to milliseconds 
 settings.net = {};
 settings.net.timeout = 0;	// <1(ms) disables default timeout (which can be set in their net-classes, too)
 settings.net.version = 6;
+settings.net.encoding = settings.encoding;
 
 settings.DEBUG = 0; // param '--debug=8' is also possible
 settings.START = true;	// general start of processes - and use this like an interpreter
 settings.START_ONLY = false;	// set to 'false' if using this as regular npm.js module! IMPORTANT!!
-
-settings.encoding = 'utf8'; // will be used for files or network connections, e.g. // maybe EXTRACT FROM '$LANG'?????
-settings.charset = 'utf-8';
 
 settings.defaultLanguage = 'en' || 'de'; // will be set by env. var. as in 'settings.variable.lang(uage)'
 
