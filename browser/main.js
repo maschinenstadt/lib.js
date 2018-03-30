41dc4e01c7f585c88e89f217912a6dbaa0ee8d14
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-30 08:47:18 +0200

    temp

--- browser/main.js
+++ browser/main.js
@@ -1,5 +1,24 @@
-function main()
+function main(_ = {})
 {
-	alert('main()');
+	var str = '';
+	for(var idx in _)
+		str += idx + '<br />\n';
+	document.write(str);
+}
+
+function table()
+{
+	//TODO/ TABELLEN-formatierung im web... wird zu formularen später..
+	//
+	//.. erstmal aber DATA-SOURCE durchdenken. ..
+	//... auch gerade @ filesys-database (via ws://!?)! ...
+	//
+	//muss auch bis zur console gebracht werden; aber auch zu web-GUIs,
+	//ob nun <canvas> oder DOM. ..
+	//
+	//
+	//am ende steht "MS ACCESS" quasi für den APP-SERVER *g*
+	//@ web-projekte. easy. infinity.
+	//
 }
 
