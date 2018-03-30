084dc1c1caf60e705666d852a14e8f71b3424185
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-30 03:32:15 +0200

    fix

--- global/Date.js
+++ global/Date.js
@@ -23,7 +23,7 @@ Object.defineProperty(Date, 'day', {
 	get: function() { return (1000 * 60 * 60 * 24); }
 });
 
-Object.defineProperty(this, 'week', {
+Object.defineProperty(Date, 'week', {
 	get: function() { return (1000 * 60 * 60 * 24 * 7); }
 });
 
