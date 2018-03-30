7d50636afc4c9f97a4d2d1279122b58fedbbb357
Sebastian 'Kuchen' Kucharczyk <geleeschaumgummi@gmail.com>
2018-03-30 12:37:53 +0200

    better. now ready!?

--- browser/init.js
+++ browser/init.js
@@ -258,9 +258,14 @@ function INIT(_ = {})
 	web.RANDOM.binary = web.util.random.binary(web.RANDOM.size);
 	web.RANDOM.hex = web.util.random.hex(web.RANDOM.size);
 	web.RANDOM.base64 = web.util.random.base64(web.RANDOM.size);
+	web.RANDOM.dual = web.util.random.dual(web.RANDOM.size);
+	web.RANDOM.decimal = web.util.random.decimal(web.RANDOM.size);
+	web.RANDOM.octal = web.util.random.octal(web.RANDOM.size);
+
 	web.RANDOM[2] = web.util.random.radix(web.RANDOM.size, 2);
+	web.RANDOM[5] = web.util.random.radix(web.RANDOM.size, 5);
 	web.RANDOM[10] = web.util.random.radix(web.RANDOM.size, 10);
-	web.RANDOM[32] = web.util.random.radix(web.RANDOM.size, 32);
+	web.RANDOM[36] = web.util.random.radix(web.RANDOM.size, 36);
 
 	var _random_size = document.getElementsByName('random_size');
 	for(var i = 0; i < _random_size.length; i++)
@@ -274,10 +279,14 @@ function INIT(_ = {})
 			+ '<li><b><big>binary</big></b> ' + web.RANDOM.binary + '</li>'
 			+ '<li><b><big>hex</big></b> ' + web.RANDOM.hex + '</li>'
 			+ '<li><b><big>base64</big></b> ' + web.RANDOM.base64 + '</li>'
+			+ '<li><b><big>dual</big></b> ' + web.RANDOM.dual + '</li>'
+			+ '<li><b><big>decimal</big></b> ' + web.RANDOM.decimal + '</li>'
+			+ '<li><b><big>octal</big></b> ' + web.RANDOM.octal + '</li>'
 			+ '<hr />'
 			+ '<li><b><big>(2)</big></b> ' + web.RANDOM[2] + '</li>'
+			+ '<li><b><big>(5)</big></b> ' + web.RANDOM[5] + '</li>'
 			+ '<li><b><big>(10)</big></b> ' + web.RANDOM[10] + '</li>'
-			+ '<li><b><big>(32)</big></b> ' + web.RANDOM[32] + '</li>'
+			+ '<li><b><big>(36)</big></b> ' + web.RANDOM[36] + '</li>'
 			+ '<hr />';
 		_random[i].innerHTML = str;
 	}
